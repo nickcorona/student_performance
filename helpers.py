07539b3f5cbc3bce47f79712b8c3f44fee805f66
Nick Corona <nicklucianocorona@gmail.com>
2021-01-05 21:28:41 -0500

    move loguniform

---
+++ helpers.py
@@ -0,0 +1,3 @@
+def loguniform(low=0, high=1, size=None, base=10):
+    """Returns a number or a set of numbers from a log uniform distribution"""
+    return np.power(base, np.random.uniform(low, high, size))
