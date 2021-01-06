50a536c2c3992e27438895f5ffadd86f2f5eef92
Nick Corona <nicklucianocorona@gmail.com>
2021-01-05 21:58:57 -0500

    import

--- helpers.py
+++ helpers.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def loguniform(low=0, high=1, size=None, base=10):
     """Returns a number or a set of numbers from a log uniform distribution"""
     return np.power(base, np.random.uniform(low, high, size))
